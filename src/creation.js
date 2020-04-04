63edce239130aaf0f4d97ba720261b2b775a2e0e
acubick <acubick@gmail.com>
2020-04-04 07:36:21 +0300

    Observable завершен

--- src/creation.js
+++ src/creation.js
@@ -1,4 +1,4 @@
-import {of, from} from 'rxjs'
+import {of, from, Observable} from 'rxjs'
 import { scan } from 'rxjs/operators'
 
 //const stream$ = of(1, 2, 3, 4)
@@ -14,4 +14,42 @@ import { scan } from 'rxjs/operators'
 //stream$.subscribe(value =>
 //	console.log('Value:', value)
 //)
-                              
+
+/*
+const stream$ = new Observable(observer => {
+
+	observer.next( 'First value')
+
+	setTimeout(() => observer.next('After 1000 ms'), 1000)
+
+	setTimeout(() => observer.complete('After 1500 ms'), 1500)
+
+//	setTimeout(() => observer.error('Something went wrong'), 2000)
+
+	setTimeout(() => observer.next('After 3000 ms'), 3000)
+
+})
+
+
+//stream$.subscribe(value => console.log('Value:', value))
+
+//stream$.subscribe(
+//	value => console.log('Value:', value),
+//	err => console.log('Err:', err),
+//	() => console.log('Complete!')
+//
+//)
+
+stream$.subscribe({
+		next(val){
+			console.log(val)
+		},
+	error(err){
+		console.log(err)
+	},
+	complete(){
+		console.log('Complete')
+	}
+})*/
+
+
