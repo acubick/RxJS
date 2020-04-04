d703b3e313f5a0f94de20dab24b3df8908c5babb
acubick <acubick@gmail.com>
2020-04-04 09:53:22 +0300

    Операторы закончены

--- src/operators.js
+++ src/operators.js
@@ -1,1 +1,36 @@
-import {interval} from 'rxjs'
+import {interval, fromEvent} from 'rxjs'
+import {
+	map, filter, tap, take, takeLast, takeWhile, scan, reduce, switchMap
+} from 'rxjs/operators'
+
+fromEvent(document, 'click')
+	.pipe(
+		switchMap(event => {
+			return interval(1000)
+				.pipe(
+					tap(v => console.log('Tap: ', v)),
+					take(5),
+					reduce((acc, v) => acc + v, 0)
+				)
+		})
+	) .subscribe({
+	next: value => console.log('Next', value),
+	complete: () => console.log('Complete')
+})
+
+const stream$ = interval(1000)
+	.pipe(
+//		tap(v => console.log('Tap: ', v)),
+//		map(value => value * 3),
+//		filter( v => v % 2 === 0),
+		take(5),
+//		takeLast(5)
+//		takeWhile(v => v < 7 )
+//		scan((acc, v) => acc + v, 0),
+		reduce((acc, v) => acc + v, 0)
+	)
+
+stream$.subscribe({
+	next: value => console.log('Next', value),
+	complete: () => console.log('Complete')
+})
