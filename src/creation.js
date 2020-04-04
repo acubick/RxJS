b4c33fc5a16e31f65fbe8691778d5edc81ba4e34
acubick <acubick@gmail.com>
2020-04-04 07:14:44 +0300

    метод from заверщен

--- src/creation.js
+++ src/creation.js
@@ -1,8 +1,17 @@
-import {of} from 'rxjs'
+import {of, from} from 'rxjs'
+import { scan } from 'rxjs/operators'
 
 //const stream$ = of(1, 2, 3, 4)
-const stream$ = of('Hello', 'World')
+//const stream$ = of('Hello', 'World')
+//stream$.subscribe(value =>
+//	console.log('Value:', value)
+//)
 
-stream$.subscribe(value => {
-	console.log('Value:', value)
-})
+//const stream$ = from([1, 2, 3, 4])
+//	.pipe(
+//		scan((acc, v) => acc.concat(v), [])
+//	)
+//stream$.subscribe(value =>
+//	console.log('Value:', value)
+//)
+                              
